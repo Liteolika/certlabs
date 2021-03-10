8dfc193186059a4a3de150283adec91f810ac451
Peter Stålbrand <peter@liteolika.se>
2021-03-10 23:37:14 +0100

    auth app

---
+++ AZ-204/Security/authapps/ConsoleApp/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    public class AppConfig
+    {
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+        public string Tenant { get; set; }
+        public string[] Scopes { get; set; }
+
+        public static AppConfig ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path);
+
+            Configuration = builder.AddUserSecrets<AppConfig>().Build();
+            return Configuration.Get<AppConfig>();
+        }
+    }
+}
---
+++ AZ-204/Security/authapps/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        static async Task Main()
+        {
+            AppConfig config = AppConfig.ReadFromJsonFile("appsettings.json");
+
+            var app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.Secret)
+                .WithAuthority(new Uri($"https://login.microsoftonline.com/{config.Tenant}"))
+                .Build();
+
+            var result = await app.AcquireTokenForClient(config.Scopes)
+                .ExecuteAsync();
+        }
+    }
+}
